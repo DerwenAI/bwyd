ef39383d8242d9c1a4385a2d395d041425e6801e
Paco Nathan <paco@derwen.ai>
2025-03-23 12:03:58 -0700

    WIP jinja2 integration

---
+++ muff.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Parse an example module in the Bwyd language.
+"""
+
+import pathlib
+import sys
+
+from icecream import ic
+import bwyd
+
+
+if __name__ == "__main__":
+    # parse an example Bwyd module
+    module: bwyd.Module = bwyd.Bwyd.parse(
+        pathlib.Path("examples/applesauce_muffins.bwyd"),
+        debug = False, # True
+    )
+
+    # interpret the parsed module
+    module.interpret(
+        debug = True, # False
+    )
+
+    #sys.exit(0)
+
+    # format as HTML
+    print(module.to_html(indent = True))
