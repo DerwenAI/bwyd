89e6d30ceb28e4b8ee5e9da6c7fe5f498ef96f5f
Paco Nathan <paco@derwen.ai>
2026-04-01 09:39:05 -0700

    WIP list

--- editor/dsl.js
+++ editor/dsl.js
@@ -1,32 +1,43 @@
 const dsl_design = [
     {
         "field": {
-	    "id": "module-title",
-	    "label": "Title",
-	    "placeholder": "My Recipe",
             "grammar": "TITLE",
+            "type": "text",
             "count": "one",
-            "type": "text"
+	    "label": "Title",
+	    "id": "module-title",
+	    "placeholder": "My Recipe",
         }
     },
     {
         "field": {
-	    "id": "module-text",
-	    "label": "Text",
-	    "placeholder": "Text Description",
             "grammar": "TEXT",
+            "type": "text",
             "count": "one",
-            "type": "text"
-
+	    "label": "Text",
+	    "id": "module-text",
+	    "placeholder": "Text Description",
 	}
-    }
+    },
+    {
+        "list": {
+            "grammar": "CITE",
+            "type": "url",
+            "count": "zero-many",
+	    "label": "Sources",
+	    "id": "cite-list",
+        }
+    },
 ]
 
 
 function build_frag (frag, design) {
+    var elem = null;
+
     for (const [kind, dat] of Object.entries(design)) {
-	if (kind == "field") {
-	    var elem = document.createElement("label");
+	switch (kind) {
+	case "field":
+	    elem = document.createElement("label");
 	    elem.setAttribute("class", "form-label");
 	    elem.setAttribute("for", dat["id"]);
 	    elem.appendChild(document.createTextNode(dat["label"]));
@@ -42,6 +53,46 @@ function build_frag (frag, design) {
 
 	    frag.appendChild(elem);
 	    console.log(elem);
+	    break;
+
+	case "list":
+	    elem = document.createElement("label");
+	    elem.setAttribute("class", "form-label");
+	    elem.setAttribute("style", "margin-top: .5em;");
+	    elem.appendChild(document.createTextNode(dat["label"]));
+
+	    frag.appendChild(elem);
+	    console.log(elem);
+
+	    elem = document.createElement("a");
+	    elem.setAttribute("class", "btn btn-primary btn-sm float-end");
+	    elem.setAttribute("href", "#");
+	    elem.setAttribute("role", "button");
+	    elem.setAttribute("style", "margin-top: .5em;");
+	    elem.appendChild(document.createTextNode("+"));
+
+	    var callback = `console.log('${dat["id"]}')`;
+	    elem.setAttribute("onclick", callback);
+
+	    frag.appendChild(elem);
+	    console.log(elem);
+
+	    elem = document.createElement("div");
+	    elem.setAttribute("class", "list-group");
+	    elem.setAttribute("id", dat["id"]);
+
+	    var sub = document.createElement("a");
+	    sub.setAttribute("class", "list-group-item disabled");
+	    sub.setAttribute("href", "#");
+	    sub.appendChild(document.createTextNode("(empty)"));
+	    elem.appendChild(sub);
+
+	    frag.appendChild(elem);
+	    console.log(elem);
+	    break;
+
+	default:
+	    break;
 	};
     };
 
