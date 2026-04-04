ef1480a2c2e9a46b878d6af05c2834e9b8f326e4
Paco Nathan <paco@derwen.ai>
2026-04-03 19:40:59 -0700

    WIP closures - added container symbols

--- editor/dsl.js
+++ editor/dsl.js
@@ -8,7 +8,7 @@ const DESIGN_META = [
             "count": "one",
 	    "id": "module-title",
 	    "label": "Title",
-	    "placeholder": "My Recipe",
+	    "placeholder": "Recipe name",
             "grammar": "TITLE",
             "type": "text",
         }
@@ -18,7 +18,7 @@ const DESIGN_META = [
             "count": "one",
 	    "id": "module-text",
 	    "label": "Text",
-	    "placeholder": "Text Description",
+	    "placeholder": "Text description",
             "grammar": "TEXT",
             "type": "text",
 	}
@@ -48,16 +48,36 @@ const DESIGN_META = [
             "count": "one-many",
 	    "id": "closure-list",
 	    "label": "Closures",
-	    "placeholder": "Text Description",
+	    "placeholder": "Text description",
             "grammar": "CLOSURE",
             "type": [
 		{
 		    "field": {
 			"count": "one",
-			"id": "closure-name-",
+			"id": "closure-name-%",
 			"label": "Name",
-			"placeholder": "Closure Name",
+			"placeholder": "Closure name",
 			"grammar": "NAME",
+			"type": "text",
+		    },
+		},
+		{
+		    "field": {
+			"count": "one",
+			"id": "closure-text-%",
+			"label": "Text",
+			"placeholder": "Closure text description",
+			"grammar": "TEXT",
+			"type": "text",
+		    },
+		},
+		{
+		    "list": {
+			"count": "one-many",
+			"id": "container-list-%",
+			"label": "Containers",
+			"placeholder": "Container symbol",
+			"grammar": "CONTAINER",
 			"type": "text",
 		    },
 		},
@@ -65,6 +85,17 @@ const DESIGN_META = [
         },
     },
 ]
+
+
+// add a UUID as a relative component to an ID
+
+function get_rel_id (id) {
+    if (id.slice(-1) === "%") {
+	return id.replace(/%/g, self.crypto.randomUUID());
+    };
+
+    return id;
+};
 
 
 // build from the given design metadata fragment,
@@ -76,24 +107,26 @@ function design_build (frag, design_meta) {
     for (const [kind, meta] of Object.entries(design_meta)) {
 	switch (kind) {
 	case "field":
+	    var item_id = get_rel_id(meta["id"]);
+
 	    elem = document.createElement("label");
 	    elem.setAttribute("class", "form-label");
-	    elem.setAttribute("for", meta["id"]);
+	    elem.setAttribute("for", item_id);
 	    elem.appendChild(document.createTextNode(meta["label"]));
 	    frag.appendChild(elem);
 
 	    elem = document.createElement("input");
-	    ELEM_META[meta["id"]] = meta;
+	    ELEM_META[item_id] = meta;
 
 	    elem.setAttribute("class", "form-control");
-	    elem.setAttribute("id", meta["id"]);
+	    elem.setAttribute("id", item_id);
 	    elem.setAttribute("type", meta["type"]);
 	    elem.setAttribute("placeholder", meta["placeholder"]);
 	    frag.appendChild(elem);
 	    break;
 
 	case "list":
-	    const group_id = meta["id"];
+	    const group_id = get_rel_id(meta["id"]);
 	    var callback = `list_add('${group_id}')`;
 
 	    elem = document.createElement("br");
@@ -102,17 +135,20 @@ function design_build (frag, design_meta) {
 	    elem = document.createElement("label");
 	    elem.setAttribute("class", "form-label");
 	    elem.setAttribute("for", group_id);
+	    elem.setAttribute("style", "display: inline-block; width: 93%;");
 	    elem.appendChild(document.createTextNode(meta["label"]));
 	    frag.appendChild(elem);
 
-	    elem = document.createElement("a");
-	    elem.setAttribute("class", "btn btn-primary btn-sm float-end");
-	    elem.setAttribute("href", "#");
-	    elem.setAttribute("role", "button");
-	    elem.appendChild(document.createTextNode("+"));
+	    var button = document.createElement("button");
+	    button.setAttribute("class", "btn btn-primary btn-sm float-end");
+	    button.setAttribute("style", "width: 2.1em;");
 
-	    elem.setAttribute("onclick", callback);
-	    frag.appendChild(elem);
+	    var icon = document.createElement("i");
+	    icon.setAttribute("class", "bi bi-plus");
+	    button.appendChild(icon);
+
+	    button.setAttribute("onclick", callback);
+	    frag.appendChild(button);
 
 	    elem = document.createElement("div");
 	    ELEM_META[group_id] = meta;
@@ -127,7 +163,7 @@ function design_build (frag, design_meta) {
 
 	    frag.appendChild(elem);
 
-	    callback = `list_del('${group_id}', 0)`;
+	    callback = `list_del("${group_id}", 0)`;
 	    CALLBACK_QUEUE.push(callback);
 	    break;
 
@@ -156,11 +192,10 @@ function list_add (group_id) {
 
     // append another element
     const meta = ELEM_META[group_id];
-    const item_id = self.crypto.randomUUID();
+    var item_id = self.crypto.randomUUID();
 
     var elem = document.createElement("div");
     elem.setAttribute("class", "row list-group-item");
-    elem.setAttribute("draggable", true);
 
     var is_structured = false;
     ELEM_META[item_id] = meta;
@@ -182,40 +217,48 @@ function list_add (group_id) {
 
 	elem.appendChild(input);
     } else {
-	// recursion to handle structured/compound types,
-	// this gets interesting
-
+	// recursion to handle structured/compound types
 	is_structured = true;
 
 	meta["type"].forEach(function(struct_meta) {
-	    elem.appendChild(
-		design_build(
-		    document.createDocumentFragment(),
-		    struct_meta,
-		)
+	    var item = design_build(
+		document.createDocumentFragment(),
+		struct_meta,
 	    );
+
+	    elem.appendChild(item);
 	});
     };
 
     var button = document.createElement("button");
-    button.setAttribute("class", "btn btn-light btn-sm float-end");
+    button.setAttribute("class", "btn btn-sm float-end");
     button.setAttribute("style", "width: 2.1em;");
 
     var icon = document.createElement("i");
     icon.setAttribute("class", "bi bi-x");
     button.appendChild(icon);
 
+    elem.setAttribute("draggable", true);
     elem.appendChild(button);
     list_group.appendChild(elem);
+
+    // for structured types, be sure to use the generated ID
+    // NB: must follow `appendChild()` calls above, or IDs won't be in the DOM
+    if (is_structured) {
+	var first_input = document.querySelector(`#${group_id} > .list-group-item > input:first-of-type`);
+	item_id = first_input.id;
+
+	button.classList.add("btn-outline-danger");
+	elem.removeChild(button);
+	elem.prepend(button);
+    } else {
+	button.classList.add("btn-light");
+    };
 
     var callback = `list_del('${group_id}', '${item_id}')`;
     button.setAttribute("onclick", callback);
 
-    // for structured types, be sure to set the generated ID
-    if (is_structured) {
-	var first_input = document.querySelector(`#${group_id} > .list-group-item > input:first-of-type`);
-	first_input.setAttribute("id", item_id);
-    };
+    process_callbacks();
 };
 
 
@@ -246,7 +289,7 @@ function list_del (group_id, item_id) {
 function encode_statement (elem, code, line) {
     const num_lines = code.split(/\r\n|\r|\n/).length;
 
-    // track HTML element vs. Bwyd script line number, for debugging
+    // track DOM element vs. Bwyd script line number, for debug
     var debug = {
 	elem_id: elem.id,
 	code: code,
@@ -272,7 +315,7 @@ function encode_module () {
     for (var i = 0; i < input_list.length; i++) {
 	var elem = input_list[i];
 	var verb = ELEM_META[elem.id].grammar;
-	var code = `${verb} "${elem.value}"`;
+	var code = `${verb}: "${elem.value}"`;
 	line = encode_statement(elem, code, line);
 	script.push(code);
     };
@@ -283,7 +326,7 @@ function encode_module () {
     for (var i = 0; i < input_list.length; i++) {
 	var elem = input_list[i];
 	var verb = ELEM_META["cite-list"].grammar;
-	var code = `${verb} "${elem.value}"`;
+	var code = `${verb}: "${elem.value}"`;
 	line = encode_statement(elem, code, line);
 	script.push(code);
     };
@@ -294,24 +337,72 @@ function encode_module () {
     for (var i = 0; i < input_list.length; i++) {
 	var elem = input_list[i];
 	var verb = ELEM_META["post-list"].grammar;
-	var code = `${verb} "${elem.value}"`;
+	var code = `${verb}: "${elem.value}"`;
 	line = encode_statement(elem, code, line);
 	script.push(code);
     };
 
-    // encode the CLOSURE list
-    input_list = document.querySelectorAll("#closure-list > .list-group-item > input");
+    // encode the CLOSURE list's compound elements
+    const closure_list = document.getElementById("closure-list").children;
 
-    for (var i = 0; i < input_list.length; i++) {
-	var elem = input_list[i];
-	var verb = ELEM_META["closure-list"].grammar;
-	var code = `${verb} "${elem.value}"`;
-	line = encode_statement(elem, code, line);
+    for (var i = 0; i < closure_list.length; i++) {
+	var closure = closure_list[i];
+	console.log("clos", closure);
+
+	var name = "";
+	var text = "";
+	var containers = [];
+	
+	for (var j = 0; j < closure.children.length; j++) {
+	    var elem = closure.children[j];
+
+	    if (elem.nodeName === "INPUT") {
+		if (elem.id.startsWith("closure-name")) {
+		    name = elem.value;
+		}
+		else if (elem.id.startsWith("closure-text")) {
+		    text = elem.value;
+		};
+	    } else if (elem.nodeName === "DIV") {
+		if (elem.id.startsWith("container-list")) {
+		    var input_list = document.querySelectorAll(`#${elem.id} > .list-group-item > input`);
+
+		    for (var k = 0; k < input_list.length; k++) {
+			containers.push(input_list[k].value);
+		    };
+		};
+	    };
+	};
+
+	var lines = [""];
+	lines.push(`CLOSURE: "${name}"`);
+	lines.push(`TEXT: "${text}"`);
+
+	for (var j = 0; j < containers.length; j++) {
+	    lines.push(`CONTAINER ${containers[j]}`);
+	};
+
+	lines.push(";");
+
+	var code = lines.join("\n");
+	line = encode_statement(closure, code, line);
 	script.push(code);
     };
 
     // update the <textarea/> script display
     document.getElementById("bwyd-script").value = script.join("\n");
+    // TODO: send update to server for validation
+};
+
+
+
+// use callbacks to clean-up after creating DOM elements
+
+function process_callbacks () {
+    while (CALLBACK_QUEUE.length > 0) {
+	const callback = CALLBACK_QUEUE.pop();
+	eval(callback);
+    };
 };
 
 
@@ -327,9 +418,5 @@ window.addEventListener("load", function() {
 	);
     };
 
-    // clean-up at end
-    while (CALLBACK_QUEUE.length > 0) {
-	const callback = CALLBACK_QUEUE.pop();
-	eval(callback);
-    };
+    process_callbacks();
 });
