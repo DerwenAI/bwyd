902c0ab05258112bc208de4d1cd292cfd78f3de4
Paco Nathan <paco@derwen.ai>
2026-07-28 09:19:12 -0700

    WIP refactor namespace to work for the entire corpus

--- demo.py
+++ demo.py
@@ -2,132 +2,159 @@
 # -*- coding: utf-8 -*-
 
 """
-An example module in the Bwyd language.
+Demo script.
 """
 
-import fileinput
-import os
 import pathlib
 import sys
-import tempfile
 import traceback
+import typing
 
 from icecream import ic
-import rdflib
 import xandergraph as xg
 
 import bwyd
 
 
-if __name__ == "__main__":
-    dsl: bwyd.Bwyd = bwyd.Bwyd(
-        config_path = pathlib.Path("config.toml"),
-    )
+class Corpus:
+    """
+Manage a corpus of Bwyd modules.
+    """
 
-    # parse a specific Bwyd module
+    def __init__ (
+        self,
+        account: str,
+        *,
+        config_path: pathlib.Path = pathlib.Path("config.toml"),
+        lang: str = "en",
+        ) -> None:
+        """
+Constructor.
+        """
+        self.lang: str = lang
+        self.account: str = account
+
+        self.dsl: bwyd.Bwyd = bwyd.Bwyd(
+            config_path = config_path,
+        )
+
+        # init the graph and load the OTTR templates
+        self.kg: xg.KnowledgeGraph = xg.KnowledgeGraph(
+            ns = {
+                "bwyd": self.dsl.config["graph"]["ns"],
+            },
+        )
+
+        graph_dir: pathlib.Path = pathlib.Path(self.dsl.config["graph"]["rdf_path"])
+        self.domain_path: pathlib.Path = graph_dir / "domain.ttl"
+        self.shapes_path: pathlib.Path = graph_dir / "shapes.ttl"
+        self.search_path: pathlib.Path = graph_dir / "search.ttl"
+        self.pantry_path: pathlib.Path = graph_dir / "pantry.ttl"
+        self.corpus_path: pathlib.Path = graph_dir / "corpus.ttl"
+
+        self.load_graph()
+        self.kg.load_stottr(graph_dir / "bwyd.stottr")
+
+
+    def load_graph (
+        self,
+        ) -> None:
+        """
+Load the graph to verify the RDF semantic descriptions.
+        """
+        path_list: list[ str ] = [
+            self.domain_path,
+            self.search_path,
+            self.pantry_path,
+        ]
+
+        for ttl_path in path_list:
+            try:
+                self.kg.graph.parse(ttl_path.as_posix())
+            except Exception as ex:
+                ic(ttl_path)
+                traceback.print_exc()
+
+
+    def iter_recipes (
+        self,
+        content_path: pathlib.Path,
+        *,
+        debug: bool = False,
+        glob: str = "*.bwyd",
+        ) -> typing.Iterator[ tuple[ str, bwyd.Recipe ]]:
+        """
+Iterate through a directory of Bwyd content.
+        """
+        ## TEMP MOCK: enumerate via pathlib.glob
+        slug_list: list[ str ] = [
+            "panna_cotta",
+            #"gravlax",
+        ]
+
+        for slug in slug_list:
+            recipe_path: pathlib.Path = content_path / f"{slug}.bwyd"
+
+            recipe: bwyd.Recipe = self.dsl.parse(
+                recipe_path,
+                slug = slug,
+                debug = debug,
+            )
+
+            # interpret the parsed module
+            recipe.interpret(
+                self.account,
+                debug = debug,
+            )
+
+            yield slug, recipe
+
+
+    def gen_rdf (
+        self,
+        content_path: pathlib.Path,
+        *,
+        debug: bool = False,
+        glob: str = "*.bwyd",
+        ) -> None:
+        """
+Iterate through a directory of Bwyd content to parse recipes and
+generate RDF.
+        """
+        iter_bwyd: typing.Iterator[ tuple[ str, bwyd.Recipe ]] = self.iter_recipes(
+            content_path,
+            debug = debug,
+            glob = glob,
+        )
+
+        for slug, recipe in iter_bwyd:
+            rdf_data: str = "\n".join(
+                recipe.gen_rdf(
+                    self.account,
+                )
+            )
+
+            self.kg.gen_ottr_rdf(rdf_data)
+
+        # serialize the full corpus as a graph in TTL format
+        with open(self.corpus_path, "w", encoding = "utf-8") as fp:
+            ttl: str = self.kg.graph.serialize(format = "turtle")
+            fp.write(ttl)
+
+        # SHACL validation
+        conforms, results_graph, results_text = self.kg.run_shacl(
+            self.corpus_path.as_posix(),
+            self.shapes_path.as_posix(),
+            self.domain_path.as_posix(),
+        )
+
+        if not conforms:
+            print(results_text)
+
+
+if __name__ == "__main__":
     account: str = "pacoid"
+    corpus: Corpus = Corpus(account)
 
     content_path: pathlib.Path = pathlib.Path("../bwyd-editor/content")
-    slug: str = sys.argv[1]
-
-    recipe: bwyd.Recipe = dsl.parse(
-        content_path / f"{slug}.bwyd",
-        slug = slug,
-        debug = False, # True
-    )
-
-    # interpret the parsed module
-    recipe.interpret(
-        account,
-        debug = False, # True
-    )
-
-    # set up OTTR templates
-    graph_dir: pathlib.Path = pathlib.Path(dsl.config["graph"]["rdf_path"])
-
-    kg: xg.KnowledgeGraph = xg.KnowledgeGraph(
-        ns = {
-            "bwyd": dsl.config["graph"]["ns"],
-        },
-    )
-
-    kg.load_stottr(graph_dir / "bwyd.stottr")
-
-    # generate RDF for a module
-    rdf_data: str = "\n".join(
-        recipe.gen_rdf(
-            account,
-        )
-    )
-
-    kg.gen_ottr_rdf(rdf_data)
-
-    corpus_path: pathlib.Path = graph_dir / "corpus.ttl"
-
-    with open(corpus_path, "w", encoding = "utf-8") as fp:
-        ttl: str = kg.graph.serialize(format = "turtle")
-        fp.write(ttl)
-
-    # load RDF from outside the DSL
-    domain_path: pathlib.Path = graph_dir / "domain.ttl"
-    shapes_path: pathlib.Path = graph_dir / "shapes.ttl"
-    search_path: pathlib.Path = graph_dir / "search.ttl"
-    pantry_path: pathlib.Path = graph_dir / "pantry.ttl"
-    entire_path: pathlib.Path = graph_dir / "entire.ttl"
-        
-    ## check for RDF syntax errors within the generated RDF
-    graph: rdflib.Graph = rdflib.Graph()
-
-    tf: tempfile.NamedTemporaryFile = tempfile.NamedTemporaryFile(
-        delete = False,
-        suffix = ".ttl",
-    )
-
-    path_list: list[ pathlib.Path ] = [
-        corpus_path,
-        search_path,
-        pantry_path,
-    ]
-
-    for ttl_path in path_list:
-        try:
-            graph.parse(ttl_path.as_posix())
-        except Exception as ex:
-            ic(ttl_path)
-            traceback.print_exc()
-
-    with open(tf.name, "w", encoding = "utf-8") as fp:
-        with fileinput.input(files = path_list, encoding = "utf-8") as stream:
-            for line in stream:
-                fp.write(line)
-
-    graph = rdflib.Graph()
-    graph.parse(tf.name)
-
-    ## SHACL validation
-    conforms, results_graph, results_text = kg.run_shacl(
-        tf.name,
-        shapes_path.as_posix(),
-        domain_path.as_posix(),
-    )
-
-    if not conforms:
-        print(results_text)
-
-    tf.close()
-    os.unlink(tf.name)
-
-    ## serialize the entire graph in TTL format
-    path_list = [
-        domain_path,
-        search_path,
-        pantry_path,
-        corpus_path,
-    ]
-
-    for ttl_path in path_list:
-        kg.graph.parse(ttl_path.as_posix())
-
-    with open(entire_path, "w", encoding = "utf-8") as fp:
-        ttl: str = kg.graph.serialize(format = "turtle")
-        fp.write(ttl)
+    corpus.gen_rdf(content_path)
